ff0c316013e08816829324c4c8e3053ec49fbc23
ShinTH <odsyjr3@naver.com>
2025-07-18 17:41:03 +0900

    backend 초기 세팅

---
+++ backend/src/test/java/com/aivle/ParkingDetection/ParkingDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.ParkingDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
