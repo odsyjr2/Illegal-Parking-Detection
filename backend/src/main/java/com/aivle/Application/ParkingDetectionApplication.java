1552ac41ea46ccb3699c0d9df1bd1c0f19b38ab7
taeeun kim <hhok70@naver.com>
2025-07-23 05:45:51 +0000

    backendapi

--- backend/src/main/java/com/aivle/Application/ParkingDetectionApplication.java
+++ backend/src/main/java/com/aivle/Application/ParkingDetectionApplication.java
@@ -1,13 +1,11 @@
-package com.aivle.ParkingDetection;
+package com.aivle.parkingdetection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParkingDetectionApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingDetectionApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingDetectionApplication.class, args);
+    }
 }
